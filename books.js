49b4c5a74bf4c562665e82b42f570cba0487fbe0
Rua <rua.elkasheef@gmail.com>
2024-02-15 22:26:18 -0500

    working on sorting and dynamic ratings deplay

--- books.js
+++ books.js
@@ -1,6 +1,18 @@
-function renderBooks() {
+function renderBooks(filter) {
   const booksWrapper = document.querySelector(".books");
+  
   const books = getBooks();
+  
+  if (filter === 'LOW_TO_HIGH'){
+      books.sort((a,b) => a.originalPrice - b.originalPrice);
+  }
+  else if (filter === 'HIGH_TO_LOW') {
+    books.sort((a,b) => b.originalPrice - a.originalPrice);
+  }
+  else if (filter === 'RATING') {
+    books.sort((a,b) => b.rating - a.rating);
+  }
+
   const booksHtml = books.map((book) => {
     return `<div class="book">
         <figure class="book__img--wrapper">
@@ -10,25 +22,36 @@ function renderBooks() {
         ${book.title}
         </div>
         <div class="book__ratings">
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star-half-alt"></i>
+         ${ratingsHTML(book.rating)}
         </div>
         <div class="book__price">
-        <span class="book__price--normal">${book.originalPrice}</span> ${book.salePrice}
+        <span>${book.originalPrice.toFixed(2)}</span>
         </div>
         </div>`;
   }).join('');
 
   booksWrapper.innerHTML = booksHtml;
-  console.log(booksHtml)
+}
+
+function ratingsHTML(rating) {
+  let ratingHTML = "" ;
+  for (let i= 0; i < Math.floor(rating) ; ++i){
+    ratingHTML += '<i class="fas fa-star"></i>\n'
+  }
+  if (!Number.isInteger(rating)) {
+    ratingHTML += '<i class="fas fa-star-half-alt"></i>\n'
+  }
+  return ratingHTML;
+}
+
+function filterBooks(event) {
+        renderBooks(event.target.value);
 }
 
 setTimeout(() => {
   renderBooks();
 });
+
 
 //FAKE CODE
 
